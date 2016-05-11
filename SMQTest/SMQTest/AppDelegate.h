599f18fa9fab1a9c05335dbdffe4a55e11ed6928
lanou3g <2251284368@qq.com>
2016-05-11 19:44:34 +0800

    This is a test. lalala

---
+++ SMQTest/SMQTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMQTest
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 宋明岐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SMQTest/SMQTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SMQTest
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 宋明岐. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
